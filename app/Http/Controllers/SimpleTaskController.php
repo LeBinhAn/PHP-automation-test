922522c18d2b74d668e7bc39eceef0c4e3df78a5
LebinhAn <lvba2102@gmail.com>
2020-12-22 13:45:06 +0700

    create simple feature test

--- app/Http/Controllers/SimpleTaskController.php
+++ app/Http/Controllers/SimpleTaskController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class SimpleTaskController extends Controller
 {
-    //
+    public function index($input)
+    {
+        //Return true if input is bigger than 10;
+        return $input > 10 ? true : false;
+    }
+
+    public function view()
+    {
+        $pagename = "Sum two number";
+
+        return view('sum', compact('pagename'));
+    }
+
+    public function sum(Request $request)
+    {
+        return $request->a + $request->b;
+    }
 }
--- tests/Feature/SimpleTaskTest.php
+++ tests/Feature/SimpleTaskTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Controllers\SimpleTaskController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SimpleTaskTest extends TestCase
 {
@@ -19,8 +23,47 @@ class SimpleTaskTest extends TestCase
 
         $response->assertStatus(200);
     }
-    public function testSimpleTaskWithCorrectInput(Request $request)
+
+    public function testSimpleTaskWithCorrectInput()
     {
-        
+        $simpleTaskController = new SimpleTaskController();
+        $passedCondition = $simpleTaskController->index(15);
+        $this->assertTrue($passedCondition);
+    }
+
+    public function testSimpleTaskWithIncorrectInput()
+    {
+        $simpleTaskController = new SimpleTaskController();
+        $passedCondition = $simpleTaskController->index(5);
+        $this->assertFalse($passedCondition);
+    }
+
+    public function testGetViewRequest()
+    {
+        $response = $this->get('/sum');
+        $response->assertStatus(200);
+    }
+
+    public function testGetViewRender()
+    {
+        $view = $this->view('sum',  ['pagename' => 'Sum two numbers']);
+        $view->assertSee('Sum two numbers');
+    }
+
+    public function testALittleMoreComplexFunction()
+    {
+        $a = 1;
+        $b = 2;
+
+        $request = Request::create('/sum', 'POST', [
+            '_token' => 'LDsqf5Sa388WtT9xUF8L7BkfBNPbiUoojeagdiVd',
+            'a' => $a,
+            'b' => $b,
+        ]);
+
+        $simpleTaskController = new SimpleTaskController();
+        $passedCondition = $simpleTaskController->sum($request) == ($a + $b);
+
+        $this->assertTrue($passedCondition);
     }
 }
